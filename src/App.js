c0a54c233207217441c73baf4343be5390d4be0f
MATIAS GONZALEZ <matigonz@uc.cl>
2023-10-26 17:38:06 -0300

    Demo frontend CI/CD

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App () {
   return (
     <div >
-      <h1>hola mundo</h1>
+      <h1>hola mundo esto es una demo</h1>
     </div>
   )
 }
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App'
 
 test('renders "Hola Mundo" on landing page', () => {
   render(<App />)
-  const greetingElement = screen.getByText(/hola mundo/i)
+  const greetingElement = screen.getByText(/hola mundo esto es una demo/i)
   expect(greetingElement).toBeInTheDocument()
 })
