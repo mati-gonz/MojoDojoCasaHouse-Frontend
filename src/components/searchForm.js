5b5292bb5b1e89ff1d0c49573631ae22c67559d0
MATIAS GONZALEZ <matigonz@uc.cl>
2023-11-13 02:25:30 -0300

    Landingpage almost complete

--- src/components/searchForm.js
+++ src/components/searchForm.js
@@ -6,9 +6,42 @@ import '../assets/styles/components/searchForm.css'
 const SearchForm = () => {
   const formRef = useRef(null)
   const [isSubmitting, setIsSubmitting] = useState(false)
+  const [useCurrentLocation, setUseCurrentLocation] = useState(false)
+  const [coordinates, setCoordinates] = useState('')
+  const [isGettingLocation, setIsGettingLocation] = useState(false)
+
+  function success (position) {
+    const latitude = position.coords.latitude
+    const longitude = position.coords.longitude
+    setCoordinates(`Latitude: ${latitude}, Longitude: ${longitude}`)
+  }
+
+  const getCurrentPosition = () => {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve, reject)
+    })
+  }
+
+  const handleLocationChange = async (event, setFieldValue) => {
+    setUseCurrentLocation(event.target.checked)
+    if (event.target.checked) {
+      setIsGettingLocation(true)
+      try {
+        const position = await getCurrentPosition()
+        success(position)
+        setFieldValue('currentLocation', true)
+      } catch (error) {
+        console.error(error)
+      }
+      setIsGettingLocation(false)
+    } else {
+      setFieldValue('currentLocation', false)
+      setCoordinates('')
+      setFieldValue('ubication', '') // Restablece el valor de 'ubication' en el formulario
+    }
+  }
 
   const searchValidationSchema = Yup.object().shape({
-    ubication: Yup.string().required('Debes ingresar una ubicación'),
     movie: Yup.string().required('Debes elegir una película')
   })
 
@@ -16,12 +49,19 @@ const SearchForm = () => {
     setIsSubmitting(true)
 
     try {
+      if (useCurrentLocation) {
+        values.ubication = coordinates
+      } else if (values.ubication === '') {
+        alert('Debes ingresar una ubicación')
+        setIsSubmitting(false)
+        return
+      }
       console.log(values)
       setIsSubmitting(false)
       resetForm()
     } catch (error) {
       setIsSubmitting(false)
-      alert('Ocurrió un error al enviar el correo')
+      alert('Ocurrió un error al buscar funciones')
     }
   }
 
@@ -29,26 +69,39 @@ const SearchForm = () => {
     <div>
         <div>
             <Formik
-                initialValues = {{ ubication: '', movie: '' }}
+                initialValues = {{ ubication: '', movie: '', currentLocation: false }}
                 validationSchema = {searchValidationSchema}
                 onSubmit = {(values, { resetForm }) => sendForm(values, { resetForm })} >
-                {({ isValid, dirty }) => (
+                {({ isValid, dirty, setFieldValue }) => (
                     <Form ref={formRef} className='formFields'>
 
-                        <label>Ingresa una ubicación</label>
-                        <Field type="input" name="ubication"/>
-                        <ErrorMessage name="ubication" component="div"/>
+                        <label className='labelText'>Ingresa una ubicación (Calle, número)</label>
+                        <Field className='inputField' type="input" name="ubication" disabled={useCurrentLocation} />
+                        <ErrorMessage className='errorStyle' name="ubication" component="div"/>
 
-                        <label>¿Qué película quieres ver?</label>
-                        <Field as="select" name="movie">
+                        <label>
+                          <Field
+                          type="checkbox"
+                          id="currentLocation"
+                          onChange={event => handleLocationChange(event, setFieldValue)}
+                          checked={useCurrentLocation}
+                          />
+                          Utilizar mi ubicación actual
+                        </label>
+
+                        <label className='labelText'>¿Qué película quieres ver?</label>
+                        <Field className='selectField' as="select" name="movie">
                             <option value="">Selecciona una película</option>
                             <option value="toy story">Toy Story</option>
                             <option value="buscando a nemo">Buscando a Nemo</option>
                             <option value="cars">Cars</option>
                         </Field>
-                        <ErrorMessage name="movie" component="div"/>
+                        <ErrorMessage className='errorStyle' name="movie" component="div"/>
 
-                        <button className='submitButton' disabled={isSubmitting || !(isValid && dirty)} type="submit">{isSubmitting ? 'Enviando...' : 'Enviar'}</button>
+                        <br/>
+                        <button className='submitButton' disabled={isSubmitting || isGettingLocation || !(isValid && dirty)} type="submit">
+                          {isSubmitting ? 'Buscando...' : isGettingLocation ? 'Obteniendo ubicación...' : 'Buscar funciones'}
+                        </button>
                     </Form>
                 )}
             </Formik>
--- src/views/Landing.js
+++ src/views/Landing.js
@@ -1,24 +1,11 @@
 import '../assets/styles/views/landing.css'
 import SearchForm from '../components/searchForm'
+import HelpMenu from '../components/helpMenu'
 
 const Landing = () => {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(success, error)
-  } else {
-    console.log('Geolocation not supported')
-  }
-
-  function success (position) {
-    const latitude = position.coords.latitude
-    const longitude = position.coords.longitude
-    console.log(`Latitude: ${latitude}, Longitude: ${longitude}`)
-  }
-
-  function error () {
-    console.log('Unable to retrieve your location')
-  }
   return (
     <div className='landingLayout'>
+        <HelpMenu />
         <div className='elementsContainers'>
             <h1>PelisCerca</h1>
             <SearchForm />
