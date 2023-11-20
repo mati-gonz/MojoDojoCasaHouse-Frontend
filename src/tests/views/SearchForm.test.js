607f8b808541c1720a8b0ed6eb035701657116cd
vicente999 <vicente.delpiano@uc.cl>
2023-11-20 18:14:31 -0300

    helptest

---
+++ src/tests/views/SearchForm.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render, cleanup, waitForElement } from '@testing-library/react';
+import "@testing-library/jest-dom";
+import axiosMock from 'axios';
+
+it("Fetches and display data", async () => {
+
+});
