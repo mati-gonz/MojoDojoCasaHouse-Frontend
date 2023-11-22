c5bde80ff519856dca531ab95858faf0c957cbe0
MATIAS GONZALEZ <matigonz@uc.cl>
2023-11-21 22:01:10 -0300

    fix linter problems

--- src/tests/views/HelpMenu.test.js
+++ src/tests/views/HelpMenu.test.js
@@ -1,21 +1,20 @@
-import { render, screen, fireEvent, act } from '@testing-library/react';
-import HelpMenu from '../../components/helpMenu';
+import { render, screen, fireEvent, act } from '@testing-library/react'
+import HelpMenu from '../../components/helpMenu'
 
 describe('<HelpMenu />', () => {
   test('should render help menu', () => {
-    render(<HelpMenu />);
-    const welcomeElement = screen.getByText('¡Bienvenido al menú de ayuda!');
-    expect(welcomeElement).toBeInTheDocument();
-  });
+    render(<HelpMenu />)
+    const welcomeElement = screen.getByText('¡Bienvenido al menú de ayuda!')
+    expect(welcomeElement).toBeInTheDocument()
+  })
 
   test('help menu appears on click', () => {
-    render(<HelpMenu />);
-    const helpButton = screen.getByText('?');
+    render(<HelpMenu />)
+    const helpButton = screen.getByText('?')
     act(() => {
-      fireEvent.click(helpButton);
-    });
-  
-    const menu = screen.getByText('¡Bienvenido al menú de ayuda!');
-    expect(menu).toBeInTheDocument();  
+      fireEvent.click(helpButton)
+    })
+    const menu = screen.getByText('¡Bienvenido al menú de ayuda!')
+    expect(menu).toBeInTheDocument()
   })
-});
+})
--- src/tests/views/SearchForm.test.js
+++ src/tests/views/SearchForm.test.js
@@ -1,86 +1,83 @@
-import React from 'react';
-import { render, cleanup, waitFor, fireEvent, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import SearchForm from '../../components/searchForm';
-import { shallow } from 'enzyme';
-import { act } from 'react-dom/test-utils';
+import { render, cleanup, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import SearchForm from '../../components/searchForm'
+import { shallow } from 'enzyme'
+import { act } from 'react-dom/test-utils'
 
-afterEach(cleanup);
+afterEach(cleanup)
 
 global.google = {
   maps: {
     places: {
-      Autocomplete: function() {
+      Autocomplete: function () {
         return {
-          addListener: jest.fn(),
-        };
+          addListener: jest.fn()
+        }
       },
       event: {
-        addListener: jest.fn(),
-      },
-    },
-  },
-};
+        addListener: jest.fn()
+      }
+    }
+  }
+}
 
 global.navigator.geolocation = {
-  getCurrentPosition: jest.fn(),
-};
+  getCurrentPosition: jest.fn()
+}
 
-const mockNavigate = jest.fn();
+const mockNavigate = jest.fn()
 
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
-  useNavigate: () => mockNavigate,
-}));
-
+  useNavigate: () => mockNavigate
+}))
 
 describe('SearchForm component', () => {
   it('renders without crashing', () => {
     // Renderiza el componente y verifica que no se caiga
-    const mockMovies = ['Movie1', 'Movie2'];
+    const mockMovies = ['Movie1', 'Movie2']
 
     // Render the component using shallow rendering
     const wrapper = shallow(
       <Router>
         <SearchForm movies={mockMovies} />
       </Router>
-    );
+    )
 
     // Assert that the component rendered successfully
-    expect(wrapper.exists()).toBe(true);
-  });
+    expect(wrapper.exists()).toBe(true)
+  })
 
   it('Rellena y envía el formulario sin errores', async () => {
-    const sendForm = jest.fn();
+    const sendForm = jest.fn()
 
     render(
       <Router>
         <SearchForm sendForm={sendForm} movies={['Película 1', 'Película 2', 'Película 3']} />
       </Router>
-    );
+    )
 
     await act(async () => {
       fireEvent.change(document.querySelector('input[placeholder="Ingresa una ubicación"]'), {
-        target: { value: 'Arturo Fernandez 006, Ovalle, Chile' },
-      });
+        target: { value: 'Arturo Fernandez 006, Ovalle, Chile' }
+      })
 
-      fireEvent.click(document.getElementById('currentLocation'));
+      fireEvent.click(document.getElementById('currentLocation'))
 
       // Select a movie
       fireEvent.change(document.querySelector('select[name="movie"]'), {
-        target: { value: 'Película 1' },
-      });
+        target: { value: 'Película 1' }
+      })
 
       fireEvent.change(document.querySelector('input[id="date"]'), {
-        target: { value: '31/12/2023' },
-      });
-      
-      console.log('Date Input Value:', document.querySelector('input[id="date"]').value);
-      
-      fireEvent.click(document.querySelector('button[type="submit"]'));
-      
-    });
+        target: { value: '31/12/2023' }
+      })
+
+      console.log('Date Input Value:', document.querySelector('input[id="date"]').value)
+
+      fireEvent.click(document.querySelector('button[type="submit"]'))
+    })
 
     // Wait for the form submission to complete
     setTimeout(() => {
@@ -89,10 +86,10 @@ describe('SearchForm component', () => {
           location: 'Arturo Fernandez 006, Ovalle, Chile',
           movie: 'Película 1',
           date: '31/12/2023',
-          currentLocation: true,
+          currentLocation: true
         },
         expect.anything()
-      );
-    }, 1000);
-  });
-});
+      )
+    }, 1000)
+  })
+})
